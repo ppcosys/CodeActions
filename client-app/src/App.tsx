da80584a8428bb9e71a99de48a6d978ccb953411
Peter <ppcosys@gmail.com>
2024-12-16 12:50:47 +0100

    Fetch first data from API

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
+  const [events, setEvents] = useState([]);
+  
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/events')
+      .then(response => {
+        setEvents(response.data)
+      })
+  }, [])
+
   return (
-    <h1>Code Actions</h1>
+    <div>
+      <Header as='h2' icon='users' content='Events' />
+      <List>
+        {events.map((event: any) => (
+          <List.Item key = {event.id}>
+            {event.title}
+          </List.Item>
+        ))}
+      </List>
+    </div>
   )
 }
 
